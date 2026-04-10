aa1db6920af2beaf454e862944322bb79d60fba8
Ryan Hess <ryanhess@users.noreply.github.com>
2026-04-10 18:05:18 -0400

    Marimo demo frontend/part1 (#2)
    * added pandas, setup demo data input table
    * editable table, serialize to JSON
    * no dataFrame for this, just a dict. This will be sent as JSON to the api
    * setup some cells and a basic waiting spinner
    * added a commit message file, not tracked.
    * Send the data using httpx to localhost backend.
    - Response should be error. For now just print the error.

--- tinker.py
+++ tinker.py
@@ -6,22 +6,73 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    suppliments_headers = ["suppliments (ingredient name)"]
+    return (suppliments_headers,)
+
+
+@app.cell
+def _(suppliments_headers):
     import marimo as mo
 
-    return
-
-
-@app.cell(hide_code=True)
-def _():
-    import httpx
-    response = httpx.get("https://api.ods.od.nih.gov/dsld/v9/label/260891")
-    label = response.json()
-    label
-    return
+    # fmt: off
+    suppliment_data = {
+        suppliments_headers[0]: [
+            "Vitamin C",
+            "Vitamin B12",
+            "Omega-3"
+        ]
+    }
+    # fmt: on
+    suppliment_data_editor = mo.ui.data_editor(
+        data=suppliment_data, label="Edit Data"
+    ).form(bordered=True)
+    suppliment_data_editor
+    return mo, suppliment_data_editor
 
 
 @app.cell
 def _():
+    import httpx
+
+    url = "http://localhost:8000/"
+
+    def get_suppliment_schedule(suppliments):
+        try:
+            response = httpx.post(url, json=suppliments)
+            response.raise_for_status()
+            suppliment_schedule = response.json()
+            return suppliment_schedule
+        except httpx.ConnectError:
+            return "Could not reach server"
+        except httpx.HTTPStatusError as e:
+            return f"{e.response.status_code}: {e.response.text}"
+        except httpx.TimeoutException:
+            return "Request timed out"
+
+    return (get_suppliment_schedule,)
+
+
+@app.cell
+def _(
+    get_suppliment_schedule,
+    mo,
+    suppliment_data_editor,
+    suppliments_headers,
+):
+    import asyncio
+    import time
+
+    edited_suppliment_data = suppliment_data_editor.value
+    suppliments = edited_suppliment_data[suppliments_headers[0]]
+
+    with mo.status.spinner(title="Sending. Waiting for response...") as spinner:
+        start_time = time.perf_counter()
+        schedule_json_or_error = get_suppliment_schedule(suppliment_data_editor.value)
+        end_time = time.perf_counter()
+        response_time = round((end_time - start_time) * 1000)
+
+    mo.output.replace(mo.md(f"Response received in {response_time}ms"))
+    mo.output.append(schedule_json_or_error)
     return
 
 
