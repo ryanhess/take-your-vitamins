38e9b2f8dba6ea57085905effb6a3eac06995374
Ryan Hess <ryanhess@users.noreply.github.com>
2026-04-10 18:17:29 -0400

    Marimo demo frontend/part3 (#4)
    * Wrote a pydantic response schema
    - Needed a suppliment request schema, wrote that as well
    - Changed the handler signature.
    
    * Return a static response
    - hard-coded a response in-schema, returned it
    - Made sure that tinker.py is passing a request body with the right shape.

--- backend.py
+++ backend.py
@@ -1,9 +1,47 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class SupplimentInPlan(BaseModel):
+    name: str
+
+
+class SupplimentPlanResponse(BaseModel):
+    before_breakfast: list[SupplimentInPlan] = []
+    after_breakfast: list[SupplimentInPlan] = []
+    before_lunch: list[SupplimentInPlan] = []
+    after_lunch: list[SupplimentInPlan] = []
+    before_dinner: list[SupplimentInPlan] = []
+    after_dinner: list[SupplimentInPlan] = []
+
+
 @app.post("/")
-async def make_viamin_schedule(suppliment_data: dict):
+async def make_viamin_schedule(
+    suppliment_data: list[SupplimentInPlan],
+) -> SupplimentPlanResponse:
+    if suppliment_data == []:
+        return SupplimentPlanResponse()
+
     print(suppliment_data)
-    raise HTTPException(status_code=500, detail="test error response")
+
+    response_schedule = SupplimentPlanResponse(
+        before_breakfast=[
+            SupplimentInPlan(name="Vitamin B12"),
+            SupplimentInPlan(name="Mugwort"),
+        ],
+        after_breakfast=[SupplimentInPlan(name="Vitamin C")],
+        before_lunch=[
+            SupplimentInPlan(name="Alpha-lipoic acid"),
+            SupplimentInPlan(name="Mugwort"),
+        ],
+        after_lunch=[SupplimentInPlan(name="Omega-3")],
+        before_dinner=[SupplimentInPlan(name="B3")],
+        after_dinner=[
+            SupplimentInPlan(name="Mugwort"),
+            SupplimentInPlan(name="Lion's Mane"),
+        ],
+    )
+
+    return response_schedule
