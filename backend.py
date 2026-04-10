e7389feefabf5de7067a173d450b0d878d93ce67
ryanhess <hess29@gmail.com>
2026-04-10 14:07:04 -0400

    Wrote a pydantic response schema - Needed a suppliment request schema, wrote that as well - Changed the handler signature.

--- backend.py
+++ backend.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class SupplimentInPlan(BaseModel):
+    name: str
+
+
+class SupplimentPlanResponse(BaseModel):
+    before_breakfast: list[SupplimentInPlan] = []
+    after_breakfast: list[SupplimentInPlan] = []
+    before_lunch: list[SupplimentInPlan] = []
+    after_lunch: list[SupplimentInPlan] = []
+    before_dinner: list[SupplimentInPlan] = []
+    after_dinner: list[SupplimentInPlan] = []
+
+
 @app.post("/")
-async def make_viamin_schedule(suppliment_data: dict):
+async def make_viamin_schedule(
+    suppliment_data: list[SupplimentInPlan],
+) -> SupplimentPlanResponse:
     print(suppliment_data)
     raise HTTPException(status_code=500, detail="test error response")
