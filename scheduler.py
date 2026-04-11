06d4c9482f64d7d95cc28708d838702828a1e179
ryanhess <hess29@gmail.com>
2026-04-10 23:25:14 -0400

    Wrote transform_to_response - also wrote a helper that gets the specific bin's worth of supplement names in a list

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from backend import (
     IngredientAttributes,
-    IngredientInPlanRequest,
+    IngredientInPlanRequestResponse,
     SupplimentPlanResponse,
 )
 
@@ -13,7 +13,7 @@ class Ingredient:
 
 
 def apply_constraints_to_sups(
-    request: list[IngredientInPlanRequest],
+    request: list[IngredientInPlanRequestResponse],
 ) -> list[Ingredient]:
     return []
 
@@ -35,18 +35,35 @@ def bin_suppliments_by_constraints(sups: list[Ingredient]) -> list[list[Ingredie
     return []
 
 
+def get_response_ingredients_from_bin(
+    bin: list[Ingredient],
+) -> list[IngredientInPlanRequestResponse]:
+    ingredients_result = [IngredientInPlanRequestResponse(name=sup.name) for sup in bin]
+    return ingredients_result
+
+
 def transform_to_response(
-    before: list[list[Ingredient]], after: list[list[Ingredient]]
+    before_bins: list[list[Ingredient]], after_bins: list[list[Ingredient]]
 ) -> SupplimentPlanResponse:
+    response = SupplimentPlanResponse(
+        before_breakfast=get_response_ingredients_from_bin(before_bins[0]),
+        before_lunch=get_response_ingredients_from_bin(before_bins[1]),
+        before_dinner=get_response_ingredients_from_bin(before_bins[2]),
+        after_breakfast=get_response_ingredients_from_bin(after_bins[0]),
+        after_lunch=get_response_ingredients_from_bin(after_bins[1]),
+        after_dinner=get_response_ingredients_from_bin(after_bins[0]),
+    )
     return SupplimentPlanResponse()
 
 
-def create_schedule(request: list[IngredientInPlanRequest]) -> SupplimentPlanResponse:
+def create_schedule(
+    request: list[IngredientInPlanRequestResponse],
+) -> SupplimentPlanResponse:
     request_sups_with_constraints = apply_constraints_to_sups(request)
     before_constrained_sups, after_constrained_sups = split_sups_before_after(
         request_sups_with_constraints
     )
     before_bins = bin_suppliments_by_constraints(before_constrained_sups)
     after_bins = bin_suppliments_by_constraints(after_constrained_sups)
-    response = transform_to_response(before=before_bins, after=after_bins)
+    response = transform_to_response(before_bins=before_bins, after_bins=after_bins)
     return response
