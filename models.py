0df8efca33d463509a01d455c3a8d920f2a87b6e
ryanhess <hess29@gmail.com>
2026-04-11 10:54:37 -0400

    Putting scheduler in a separate file was causing a circular import. - Instead of putting everything into one file, extracted models and sample-data. - Verified no errors related to circular import

---
+++ models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Literal
+
+
+class IngredientAttributes(BaseModel):
+    take_not_with: list[str] = []
+    before_after_food: Literal["before", "after"]
+
+
+class IngredientInRequest(BaseModel):
+    name: str
+
+
+class IngredientInResponse(BaseModel):
+    name: str
+    DEV__conflict_count: int = 0
+
+
+class SupplimentPlanResponse(BaseModel):
+    before_breakfast: list[IngredientInResponse] = []
+    after_breakfast: list[IngredientInResponse] = []
+    before_lunch: list[IngredientInResponse] = []
+    after_lunch: list[IngredientInResponse] = []
+    before_dinner: list[IngredientInResponse] = []
+    after_dinner: list[IngredientInResponse] = []
+    DEV_total_conflict_count: int = 0
