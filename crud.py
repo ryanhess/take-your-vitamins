53ac29fe3b79e0a09a023fe229be5d6d329904b1
ryanhess <hess29@gmail.com>
2026-05-01 07:49:12 -0400

    Write the update method for the schedule.

--- crud.py
+++ crud.py
@@ -1,4 +1,9 @@
-from models import TimeSlots
+from models import (
+    IngredientInSchedule,
+    SupplementSchedule,
+    TimeSlots,
+)
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -9,4 +14,24 @@ class Schedule:
 
     @staticmethod
     async def update(sched_id: int, updated_sched: TimeSlots, db: AsyncSession) -> None:
+        sched = await db.get(SupplementSchedule, sched_id)
+        if sched is None:
+            return None
+
+        ingreds_in_sched = []
+
+        for slot in updated_sched.__dict__.items():
+            for ingred in slot[1]:
+                # fmt: off
+                new_sched_entry = IngredientInSchedule(
+                    ingredient_id=ingred.id,
+                    schedule_id=sched_id,
+                    slot=slot[0]
+                )
+                # fmt: on
+
+                ingreds_in_sched.append(new_sched_entry)
+
+        db.add_all(ingreds_in_sched)
+        await db.commit()
         return None
