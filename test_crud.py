2d541acca8cb6c11090a443b0340a2a32c9aa76d
ryanhess <hess29@gmail.com>
2026-05-01 09:32:42 -0400

    Add empty stubs for update tests

---
+++ test_crud.py
@@ -0,0 +1,15 @@
+class TestUpdateSchedule:
+    def test_raises_for_not_found(self) -> None:
+        pass
+
+    def test_raises_for_ingred_not_found(self) -> None:
+        pass
+
+    def test_update_with_empty_clears_schedule(self) -> None:
+        pass
+
+    def test_idempotent(self) -> None:
+        pass
+
+    def test_updates_schedule_in_db(self) -> None:
+        pass
