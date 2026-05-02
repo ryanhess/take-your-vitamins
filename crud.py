27774da0353b6571aa876085cfb3dd350f5a3d45
ryanhess <hess29@gmail.com>
2026-05-02 15:16:31 -0400

    In update: - extract another helper to turn the schedule item to a list of IngredientInSchedule. Better readability

--- crud.py
+++ crud.py
@@ -7,6 +7,25 @@ from sqlalchemy import text, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
+def _ingreds_from_time_slots(
+    sched_id: int, slots: TimeSlots
+) -> list[IngredientInSchedule]:
+    ingredients = []
+    for slot in slots.__dict__.items():
+        for ingred in slot[1]:
+            # fmt: off
+            new_sched_entry = IngredientInSchedule(
+                ingredient_id=ingred.id,
+                schedule_id=sched_id,
+                slot=slot[0]
+            )
+            # fmt: on
+
+            ingredients.append(new_sched_entry)
+
+    return ingredients
+
+
 class Schedule:
     @staticmethod
     async def get(sched_id: int, db: AsyncSession) -> TimeSlots:
@@ -19,23 +38,14 @@ class Schedule:
         if sched is None:
             return None
 
-        ingreds_in_new_sched = []
-
-        for slot in updated_sched.__dict__.items():
-            for ingred in slot[1]:
-                # fmt: off
-                new_sched_entry = IngredientInSchedule(
-                    ingredient_id=ingred.id,
-                    schedule_id=sched_id,
-                    slot=slot[0]
-                )
-                # fmt: on
-
-                ingreds_in_new_sched.append(new_sched_entry)
+        ingreds_in_new_sched = _ingreds_from_time_slots(
+            sched_id=sched_id, slots=updated_sched
+        )
 
         del_stmt = delete(IngredientInSchedule).where(
             IngredientInSchedule.schedule_id == sched_id
         )
         await db.execute(del_stmt)
+
         db.add_all(ingreds_in_new_sched)
         await db.commit()
