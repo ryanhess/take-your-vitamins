899d7906a677a4b1fd868f6e01fc405a526409f3
ryanhess <hess29@gmail.com>
2026-05-04 14:31:24 -0400

    add the get schedule route.

--- backend.py
+++ backend.py
@@ -1,8 +1,10 @@
 from database import AsyncDb
-from fastapi import FastAPI
-from models import ScheduleRequest, SupplementPlanResponse
+from fastapi import FastAPI, HTTPException
+from models import ScheduleRequest, SupplementPlanResponse, SupplementSchedule
 from fastapi.middleware.cors import CORSMiddleware
 import scheduler
+from sqlalchemy import select
+from crud import Schedule
 
 
 app = FastAPI()
@@ -27,3 +29,16 @@ async def make_viamin_schedule(
     response_schedule = await scheduler.create_schedule(supplement_set, db)
 
     return response_schedule
+
+
+@app.get("/schedule/get")
+async def get_schedule_handler(user_id: int, db: AsyncDb) -> SupplementPlanResponse:
+    query = select(SupplementSchedule.id).where(SupplementSchedule.user_id == 1)
+    result = await db.execute(query)
+    schedule_id = result.scalar_one_or_none()
+    if schedule_id is None:
+        raise HTTPException(status_code=404, detail="Schedule not found for user")
+
+    response_schedule = await Schedule.get(sched_id=schedule_id, db=db)
+    response = SupplementPlanResponse(schedule=response_schedule)
+    return response
